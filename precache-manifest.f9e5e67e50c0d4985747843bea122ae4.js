ef3c51546d03868e3f027eaf4f5cfbeeeeffb993
trevor <trevor.winger@artunlimitedusa.com>
2020-08-31 08:51:21 -0500

    Updates

---
+++ precache-manifest.f9e5e67e50c0d4985747843bea122ae4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93ae13c0447cd56eb566593063b1168a",
+    "url": "/content-autofy/index.html"
+  },
+  {
+    "revision": "3aece9f47491bcc6580c",
+    "url": "/content-autofy/static/css/main.4d2a7988.chunk.css"
+  },
+  {
+    "revision": "acbb7854ac2ffe7c1fdb",
+    "url": "/content-autofy/static/js/2.12bcf66b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/content-autofy/static/js/2.12bcf66b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aece9f47491bcc6580c",
+    "url": "/content-autofy/static/js/main.8e2735db.chunk.js"
+  },
+  {
+    "revision": "651485775303f323fcdd",
+    "url": "/content-autofy/static/js/runtime-main.17324fff.js"
+  }
+]);
